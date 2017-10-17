df84f2fa0ee4ff87919ab7def7b0d1965a718887
luyanmin <luyanmin@yunbi.com>
2017-10-17 17:29:09 +0800

    init commit

---
+++ .iex.exs
@@ -0,0 +1,5 @@
+import Ecto.Query
+alias Pxblog.User
+alias Pxblog.Repo
+alias Pxblog.Post
+import Ecto
---
+++ web/models/post.ex
@@ -0,0 +1,22 @@
+defmodule Pxblog.Post do
+  use Pxblog.Web, :model
+
+  schema "posts" do
+    belongs_to :user, Pxblog.User
+
+    field :title, :string
+    field :body, :string
+
+    timestamps()
+  end
+
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title, :body])
+    |> validate_required([:title, :body])
+  end
+end
